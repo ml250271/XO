67b224dbd8a975490e549c79ef307c7c77b812c5
ml250271 <lukicmilica@hotmail.com>
2017-12-18 21:27:41 +0100

    writing XO corrected

--- js/XO.js
+++ js/XO.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
         var cells;
         var history = [];
-        
+        var count = 0;
+    
         function refresh() {
             $("#playground").empty("table");
             var table = $("<table>")
@@ -23,18 +24,25 @@ $(document).ready(function() {
                 var tdid;
                 td.click(function() {
                     
+                    count++;
+                   
                     tdid = $(this).attr("id").split("_")[1];
+                    /* if ( $.inArray(history,this)) {
+                        console.log("lol");
+                        }*/
                     var cellId = Number(tdid) + 1;
                     history.push(cellId);                 
                     console.log(history);
-                 if ( i % 2 == 0 ) {
-                        $(this).text("X");
-                    } else {
+                 if ( count % 2 == 0 ) {
                         $(this).text("O");
+                    } else {
+                        $(this).text("X");
                     }
+                    
                     
                                       
                 })
+                
             });
             $("#playground").append(table);
             
